292176e75197d6cd14d607c5f726192baccdc8d3
zinntikumugai <uesitananame55@gmail.com>
2019-03-10 17:07:38 +0900

    シンプルにこれだけ(雑

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,98 @@
 <template>
   <section class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        blockbook.ovh
-      </h1>
-      <h2 class="subtitle">
-        My luminous Nuxt.js project
-      </h2>
+      <h1 class="title">blockbook.ovh</h1>
+      <h2 class="subtitle">blockbook hosted by zinntikumugai</h2>
+      <div>
+        <b-card-group columns>
+          <div v-for="coin in coins" :key="coin">
+            <b-link :href="coin.url">
+              <b-card :title="coin.name">
+                <b-img :src="coin.imgurl" width="200"/>
+              </b-card>
+            </b-link>
+          </div>
+        </b-card-group>
+      </div>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <a href="https://twitter.com/uesitananame55" target="_blank" class="button--green">About</a>
+        <a href="https://github.com/zinntikumugai/blockbook" target="_blank" class="button--grey">GitHub</a>
+        <a href="https://github.com/trezor/blockbook" target="_blank" class="button--grey">Orignal GitHub</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  components: {},
+  data() {
+    return {
+      coins: [
+        {
+          name: "BitZeny",
+          url: "https://zny.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
+          test: false
+        },
+        {
+          name: "BitZeny(Testnet)",
+          url: "https://test-zny.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
+          test: true
+        },
+        {
+          name: "Bellcoin",
+          url: "https://bell.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/bellcoin-org/bellcoin@master/src/qt/res/src/bitcoin.svg?raw=true",
+          test: false
+        },
+        {
+          name: "Koto",
+          url: "https://koto.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
+          test: false
+        },
+        {
+          name: "Koto(Testnet)",
+          url: "https://test-koto.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
+          test: true
+        },
+        {
+          name: "Monacoin",
+          url: "https://mona.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/monacoinproject/monacoin@master-0.16/src/qt/res/src/bitcoin.svg?raw=true",
+          test: false
+        },
+        {
+          name: "Susucoin",
+          url: "https://susu-blockbook.currency.ovh",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/susucoin-project/susucoin@master/src/qt/res/icons/bitcoin.png?raw=true",
+          test: false
+        },
+        {
+          name: "Susucoin(Testnet)",
+          url: "https://testnet-susu-blockbook.currency.ovh",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/susucoin-project/susucoin@master/src/qt/res/icons/bitcoin.png?raw=true",
+          test: true
+        }
+      ].sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      })
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -45,8 +106,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
