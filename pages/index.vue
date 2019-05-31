fd93c6afd092320487e3456759d02fb8d2a44699
Takuma Shinohara <takuma1201@gmail.com>
2019-06-01 03:23:25 +0900

    デザイン変更（簡単に手抜き）

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,40 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">blockbook.ovh</h1>
-      <h2 class="subtitle">blockbook hosted by zinntikumugai</h2>
-      <div>
-        <b-card-group columns>
-          <div v-for="coin in coins" :key="coin">
-            <b-link :href="coin.url">
-              <b-card :title="coin.name">
-                <b-img :src="coin.imgurl" width="200"/>
-              </b-card>
-            </b-link>
-          </div>
-        </b-card-group>
-      </div>
-      <div class="links">
-        <a href="https://twitter.com/uesitananame55" target="_blank" class="button--green">About</a>
-        <a href="https://github.com/zinntikumugai/blockbook" target="_blank" class="button--grey">GitHub</a>
-        <a href="https://github.com/trezor/blockbook" target="_blank" class="button--grey">Orignal GitHub</a>
-      </div>
-      <div>
-        &copy; 2019 blockbook.ovh
+  <div class="container">
+    <h1 class="text-center mt-3">blockbook.ovh</h1>
+    <p class="text-right mb-1">blockbook hosted by @zinntikumugai</p>
+    <h4 class="list-title">Main Net</h4>
+    <div class="row p-2 pb-3">
+      <div class="col-6 col-md-4 col-lg-3 p-2" v-for="coin in coinsMainNet" :key="coin">
+        <div class="pb-3 text-center">
+          <h4 class>{{coin.name}}</h4>
+          <b-img :src="coin.imgurl" width="120"/>
+        </div>
       </div>
     </div>
-  </section>
+    <h3 class="list-title">Test Net</h3>
+    <div class="row p-2 pb-5">
+      <div class="col-6 col-md-4 col-lg-3 p-2" v-for="coin in coinsTestNet" :key="coin">
+        <div class="pb-3 text-center">
+          <h4>{{coin.name}}</h4>
+          <b-img :src="coin.imgurl" width="120"/>
+        </div>
+      </div>
+    </div>
+    <div class="text-center pb-5">
+      <a href="https://twitter.com/uesitananame55" target="_blank" class="btn bg-twitter text-white"><i class="fab fa-twitter fa-fw"></i> @uesitananame55</a>
+      <a
+        href="https://github.com/zinntikumugai/blockbook"
+        target="_blank"
+        class="btn bg-github text-white"
+      ><i class="fab fa-github fa-fw"></i> GitHub</a>
+      <a
+        href="https://github.com/trezor/blockbook"
+        target="_blank"
+        class="btn bg-github text-white"
+      ><i class="fab fa-github fa-fw"></i> Orignal GitHub</a>
+    </div>
+    <div class="pb-5 text-center">&copy; 2019 blockbook.ovh</div>
+  </div>
 </template>
 
 <script>
@@ -31,20 +42,13 @@ export default {
   components: {},
   data() {
     return {
-      coins: [
+      coinsMainNet: [
         {
           name: "BitZeny",
           url: "https://zny.blockbook.ovh/",
           imgurl:
             "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
           test: false
-        },
-        {
-          name: "BitZeny(Testnet)",
-          url: "https://test-zny.blockbook.ovh/",
-          imgurl:
-            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
-          test: true
         },
         {
           name: "Bellcoin",
@@ -59,13 +63,6 @@ export default {
           imgurl:
             "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
           test: false
-        },
-        {
-          name: "Koto(Testnet)",
-          url: "https://test-koto.blockbook.ovh/",
-          imgurl:
-            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
-          test: true
         },
         {
           name: "Monacoin",
@@ -85,6 +82,26 @@ export default {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
         return 0;
+      }),
+      coinsTestNet: [
+        {
+          name: "BitZeny",
+          url: "https://test-zny.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
+          test: true
+        },
+        {
+          name: "Koto",
+          url: "https://test-koto.blockbook.ovh/",
+          imgurl:
+            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
+          test: true
+        }
+      ].sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
       })
     };
   }
@@ -92,34 +109,8 @@ export default {
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+h4 {
+  font-weight: normal;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
