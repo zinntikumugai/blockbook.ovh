f649ebf88efe143c9fa229c24298bafd34433507
zinntikumugai <uesitananame55@gmail.com>
2019-07-12 11:47:28 +0900

    add uptimerobot

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,13 @@
         <div class="col-6 col-md-4 col-lg-3 p-2" v-for="coin in coinsMainNet" :key="coin">
           <div class="pb-3 text-center">
             <b-link :href="coin.url">
-              <h4 class>{{coin.name}}</h4>
+              <h4 class>
+                {{coin.name}}
+                <b-img
+                  v-if="coin.uptime"
+                  :src="`https://img.shields.io/uptimerobot/ratio/${coin.uptime}.svg?style=flat-square&label=`"
+                />
+              </h4>
               <b-img :src="coin.imgurl" width="120" />
             </b-link>
           </div>
@@ -27,7 +33,13 @@
         <div class="col-6 col-md-4 col-lg-3 p-2" v-for="coin in coinsTestNet" :key="coin">
           <div class="pb-3 text-center">
             <b-link :href="coin.url">
-              <h4>{{coin.name}}</h4>
+              <h4>
+                {{coin.name}}
+                <b-img
+                  v-if="coin.uptime"
+                  :src="`https://img.shields.io/uptimerobot/ratio/${coin.uptime}.svg?style=flat-square&label=`"
+                />
+              </h4>
               <b-img :src="coin.imgurl" width="120" />
             </b-link>
           </div>
@@ -84,25 +96,29 @@ export default {
           name: "BitZeny",
           url: "https://zny.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true"
+            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
+          uptime: "m782479834-70ab5a9ca84ea2dd1b1d534c"
         },
         {
           name: "Bellcoin",
           url: "https://bell.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/bellcoin-org/bellcoin@master/src/qt/res/src/bitcoin.svg?raw=true"
+            "https://cdn.jsdelivr.net/gh/bellcoin-org/bellcoin@master/src/qt/res/src/bitcoin.svg?raw=true",
+          uptime: "m782479833-12e6fb4c51b7505ab0f13e6a"
         },
         {
           name: "Koto",
           url: "https://koto.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true"
+            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
+          uptime: "m782479838-f3e966f5ebbbbee971e540ae"
         },
         {
           name: "Monacoin",
           url: "https://mona.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/monacoinproject/monacoin@master-0.16/src/qt/res/src/bitcoin.svg?raw=true"
+            "https://cdn.jsdelivr.net/gh/monacoinproject/monacoin@master-0.16/src/qt/res/src/bitcoin.svg?raw=true",
+          uptime: "m782479841-6324434b71f0872b2b53d0d5"
         }
       ].sort(sort),
       coinsTestNet: [
@@ -110,13 +126,15 @@ export default {
           name: "BitZeny",
           url: "https://test-zny.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true"
+            "https://cdn.jsdelivr.net/gh/bitzenyPlus/BitZenyPlus@z2.0.x/src/qt/res/icons/bitcoin.png?raw=true",
+          uptime: "m782479835-b72b74e9b0bcdc870bcf78da"
         },
         {
           name: "Koto",
           url: "https://test-koto.blockbook.ovh/",
           imgurl:
-            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true"
+            "https://cdn.jsdelivr.net/gh/KotoDevelopers/koto@master/src/qt/res/icons/koto.png?raw=true",
+          uptime: "m782479840-bc3044c286c19197593f7cbf"
         }
       ].sort(sort)
     };
